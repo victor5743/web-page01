f33b3e9e9649a3d968407e4cb982802588136339
Victor <vcs5743@gmail.com>
2020-05-04 17:02:16 -0400

    Update PanelLeft component

--- src/components/PanelLeft.vue
+++ src/components/PanelLeft.vue
@@ -1,5 +1,253 @@
 <template>
-	<div class="block w-1/4 h-screen"></div>
+	<div class="block w-1/4 overflow-y-auto border-r-2 border-gray-400 overflow-auto">
+		<ul class="text-sm border-b-2 border-gray-400">
+			<a href="#">
+				<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+					<div class="flex items-center text-gray-800">
+						<svg
+							class="fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="24"
+							height="24"
+						>
+							<path
+								class="heroicon-ui"
+								d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM5.68 7.1A7.96 7.96 0 0 0 4.06 11H5a1 1 0 0 1 0 2h-.94a7.95 7.95 0 0 0 1.32 3.5A9.96 9.96 0 0 1 11 14.05V9a1 1 0 0 1 2 0v5.05a9.96 9.96 0 0 1 5.62 2.45 7.95 7.95 0 0 0 1.32-3.5H19a1 1 0 0 1 0-2h.94a7.96 7.96 0 0 0-1.62-3.9l-.66.66a1 1 0 1 1-1.42-1.42l.67-.66A7.96 7.96 0 0 0 13 4.06V5a1 1 0 0 1-2 0v-.94c-1.46.18-2.8.76-3.9 1.62l.66.66a1 1 0 0 1-1.42 1.42l-.66-.67zM6.71 18a7.97 7.97 0 0 0 10.58 0 7.97 7.97 0 0 0-10.58 0z"
+							/>
+						</svg>
+						<span class="ml-2">Dashboard</span>
+					</div>
+				</li>
+			</a>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M8 4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h2zm0 2H6v14h12V6h-2a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2zm2-2v2h4V4h-4z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Friendly Form</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 0 1-1.79 1.1h-2.76a2 2 0 0 1-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 0 1 1.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 0 1 1.79-1.1H19z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Friendly Submissions</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Properties</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M9 12A5 5 0 1 1 9 2a5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H7a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v2zm1-5a1 1 0 0 1 0-2 5 5 0 0 1 5 5v2a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3zm-2-4a1 1 0 0 1 0-2 3 3 0 0 0 0-6 1 1 0 0 1 0-2 5 5 0 0 1 0 10z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Contacts</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M18 21H7a4 4 0 0 1-4-4V5c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2h2a2 2 0 0 1 2 2v11a3 3 0 0 1-3 3zm-3-3V5H5v12c0 1.1.9 2 2 2h8.17a3 3 0 0 1-.17-1zm-7-3h4a1 1 0 0 1 0 2H8a1 1 0 0 1 0-2zm0-4h4a1 1 0 0 1 0 2H8a1 1 0 0 1 0-2zm0-4h4a1 1 0 0 1 0 2H8a1 1 0 1 1 0-2zm9 11a1 1 0 0 0 2 0V7h-2v11z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">AOAs</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M11 14.59V3a1 1 0 0 1 2 0v11.59l3.3-3.3a1 1 0 0 1 1.4 1.42l-5 5a1 1 0 0 1-1.4 0l-5-5a1 1 0 0 1 1.4-1.42l3.3 3.3zM3 17a1 1 0 0 1 2 0v3h14v-3a1 1 0 0 1 2 0v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Imports</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M13 5.41V17a1 1 0 0 1-2 0V5.41l-3.3 3.3a1 1 0 0 1-1.4-1.42l5-5a1 1 0 0 1 1.4 0l5 5a1 1 0 1 1-1.4 1.42L13 5.4zM3 17a1 1 0 0 1 2 0v3h14v-3a1 1 0 0 1 2 0v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Exports</a>
+				</div>
+			</li>
+		</ul>
+		<ul class="text-sm">
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M4 16v5a1 1 0 0 1-2 0V3a1 1 0 0 1 1-1h8.5a1 1 0 0 1 .7.3l.71.7H21a1 1 0 0 1 .9 1.45L19.11 10l2.77 5.55A1 1 0 0 1 21 17h-8.5a1 1 0 0 1-.7-.3l-.71-.7H4zm7-12H4v10h7.5a1 1 0 0 1 .7.3l.71.7h6.47l-2.27-4.55a1 1 0 0 1 0-.9L19.38 5H13v4a1 1 0 0 1-2 0V4z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Property Flag</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M19 10v6a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2c0-1.1.9-2 2-2v-6a2 2 0 0 1-2-2V7a1 1 0 0 1 .55-.9l8-4a1 1 0 0 1 .9 0l8 4A1 1 0 0 1 21 7v1a2 2 0 0 1-2 2zm-6 0h-2v6h2v-6zm4 0h-2v6h2v-6zm-8 0H7v6h2v-6zM5 7.62V8h14v-.38l-7-3.5-7 3.5zM5 18v2h14v-2H5z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">CAD Websites</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M7.7 6.3L9 7.58l4.3-4.3a1 1 0 0 1 1.4 1.42L10.42 9l4.3 4.3a1 1 0 0 1-1.42 1.4L9 10.42l-1.3 1.3a3 3 0 1 1-1.4-1.42L7.58 9l-1.3-1.3a3 3 0 1 1 1.42-1.4zM21 8a1 1 0 0 1 1 1v1a1 1 0 0 1-2 0 1 1 0 0 1 0-2h1zM4 20a1 1 0 0 1 0 2H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 2 0zm17 2h-1a1 1 0 0 1 0-2 1 1 0 0 1 2 0v1a1 1 0 0 1-1 1zM14 8h2a1 1 0 0 1 0 2h-2a1 1 0 0 1 0-2zm7 5a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1zm-7 7h2a1 1 0 0 1 0 2h-2a1 1 0 0 1 0-2zm-6 0h2a1 1 0 0 1 0 2H8a1 1 0 0 1 0-2zm-2.3-6.3a1 1 0 1 0-1.4-1.4 1 1 0 0 0 1.4 1.4zM5 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Scrapers</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M13 16v5a1 1 0 0 1-1 1H9l-3-6a2 2 0 0 1-2-2 2 2 0 0 1-2-2v-2c0-1.1.9-2 2-2 0-1.1.9-2 2-2h7.59l4-4H20a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-2.41l-4-4H13zm0-2h1.41l4 4H20V4h-1.59l-4 4H13v6zm-2 0V8H6v2H4v2h2v2h5zm0 2H8.24l2 4H11v-4z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Updates</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M13 17h-2v2h2v-2zm2 0v2h2a1 1 0 0 1 0 2H7a1 1 0 0 1 0-2h2v-2H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-5zM4 5v10h16V5H4z"
+						/>
+					</svg>
+					<a class="ml-2" href="#">Resolute PTS Website</a>
+				</div>
+			</li>
+			<li class="items-center py-2 pl-4 mb-1 hover:bg-gray-400">
+				<div class="flex items-center text-gray-800">
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+					>
+						<path
+							d="M9.41,11H21c0.553,0,1,0.448,1,1c0,0.553-0.447,1-1,1H9.41l3.3,3.3c0.356,0.422,0.304,1.054-0.118,1.409
+  c-0.377,0.318-0.93,0.314-1.302-0.009l-5-5c-0.382-0.389-0.382-1.011,0-1.4l5-5c0.417-0.362,1.048-0.318,1.411,0.099
+  c0.323,0.373,0.327,0.925,0.009,1.302L9.41,11z M7,3c0.553,0,1,0.448,1,1S7.553,5,7,5H4v14h3c0.553,0,1,0.447,1,1s-0.447,1-1,1H4
+  c-1.104,0-2-0.896-2-2V5c0-1.104,0.896-2,2-2H7z"
+						/>
+					</svg>
+
+					<a class="ml-2" href="#">Logout</a>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
