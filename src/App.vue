ee4c6a04a054aef0f460435befd6f60e193ded0e
Victor <vcs5743@gmail.com>
2020-05-03 22:56:53 -0400

    PanelLeft component added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
 	<div id="app">
 		<Search />
+
+		<!-- Panels -->
+		<div class="flex">
+			<PanelLeft />
+		</div>
 	</div>
 </template>
 
 <script>
 import Search from './components/Search.vue';
+import PanelLeft from './components/PanelLeft.vue';
 
 export default {
 	name: 'App',
 	components: {
 		Search,
+		PanelLeft,
 	},
 };
 </script>
---
+++ src/components/PanelLeft.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="block w-1/4 h-screen"></div>
+</template>
+
+<script>
+export default {};
+</script>
