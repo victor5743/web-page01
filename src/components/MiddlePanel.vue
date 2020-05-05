46d8537c670bfd782f7021d352b9d6ac3ed56d91
Victor <vcs5743@gmail.com>
2020-05-04 22:15:23 -0400

    Heading section added in MiddlePanel component

--- src/components/MiddlePanel.vue
+++ src/components/MiddlePanel.vue
@@ -1,5 +1,38 @@
 <template>
-	<div></div>
+	<div class="block w-1/2s">
+		<div class="px-4 py-2 border-b-2 border-gray-400 text-sm">
+			<span class="text-secondary"><a href="#">Contacts</a></span>
+			<span class="mx-2">></span>
+			<span class="text-secondary"><a href="#">User</a></span>
+			<span class="mx-2">></span>
+			<span>Properties</span>
+		</div>
+		<div class="px-4 mt-2">
+			<!-- Heading section -->
+			<div class="flex text-secondary items-center">
+				<div>
+					<svg
+						class="fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+					>
+						<path
+							class="heroicon-ui"
+							d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"
+						/>
+					</svg>
+				</div>
+				<h1 class="ml-2 pt-1 text-xl uppercase text-gray-800">Details User</h1>
+				<div
+					class="bg-secondary text-white px-4 py-1 mt-1 rounded-full text-xs uppercase tracking-wide ml-2 font-semibold"
+				>
+					Active
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
